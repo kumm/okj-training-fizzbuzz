31cc53220d507c26e5bbf860da807899d429e50f
kumm <kumm@webstar.hu>
2017-11-18 14:31:55 +0100

    fizzbuzzer1

--- src/main/java/com/wcs/fizzbuzz/FizzBuzzer.java
+++ src/main/java/com/wcs/fizzbuzz/FizzBuzzer.java
@@ -1,9 +1,57 @@
 package com.wcs.fizzbuzz;
 
 public class FizzBuzzer {
-    
-    public String execute(int number) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+    public String executeOld(int number) {
+
+
+        if (number % 5 == 0 && number % 3 == 0) {
+            return "fizzbuzz";
+        } else {
+            if (number % 3 == 0) {
+                return "fizz";
+            } else if (number % 5 == 0) {
+                return "buzz";
+            } else
+
+                return "" + number;
+        }
+
     }
-    
+
+    public String executeold2(int number) {
+        String vissza = "";
+        if (number % 3 == 0) {
+            vissza += "fizz";
+        }
+        if (number % 5 == 0) {
+            vissza += "buzz";
+        }
+        if (vissza.isEmpty()) {
+            vissza = "" + number;
+        }
+
+        return vissza;
+
+
+    }
+
+
+    public String execute(int number) {
+        StringBuilder vissza = new StringBuilder();
+        if (number % 3 == 0) {
+            vissza.append("fizz");
+        }
+        if (number % 5 == 0) {
+            vissza.append("buzz");
+        }
+        if (vissza.length() == 0) {
+            vissza.append(number);
+        }
+
+        return vissza.toString();
+
+
+    }
+
 }
